c11be03b302980f8a343f6f53fc67cd7d7a68b5c
Maredi01 <92855864+Maredi01@users.noreply.github.com>
2022-10-05 00:50:02 +0200

    Delete Constants.java

--- Constants.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class Constants {
-	 public static final String URL = "https://www.sovtech.co.za/contact-us/";
-	    public static final String Path_TestData = "C:\\Users\\Patricia Matjipa\\";
-	    public static final String File_TestData = "Sovtech.xlsx";
-}
